d882ef16ea03d8e44293518215a7edf4ec002814
NoOneImportant <restaldt@NoOneImportants-MacBook-Air.local>
2017-07-18 04:48:55 -0500

    Ball moves position based on a position array

--- Scroller/Scroller.swift
+++ Scroller/Scroller.swift
@@ -12,6 +12,7 @@ import GameplayKit
 
 class Scroller: SKScene, SKPhysicsContactDelegate {
     
+    var entities = [GKEntity]()
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     var ball: SKSpriteNode!
@@ -19,11 +20,48 @@ class Scroller: SKScene, SKPhysicsContactDelegate {
     var lastPosition: CGPoint!
     private var spinnyNode : SKShapeNode?
     var count = 0
+    var posArr = Array(repeating: CGPoint(), count:1000)
+    
+    
+    override func sceneDidLoad() {
+        ball = childNode(withName: "ball") as! SKSpriteNode
+        
+       
+        
+        // Get label node from scene and store it for use later
+        /*
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+            if let label = self.label {
+                label.alpha = 0.0
+                label.text = "Tap to Begin"
+                label.run(SKAction.fadeIn(withDuration: 2.0))
+            }
+ */
+   
+        // Create shape node to use during mouse interaction
+       // let w = (self.size.width + self.size.height) * 0.05
+        let w = 2.5
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: 0.4)
+        
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            
+         
+        }
+
+    }
+
     func touchMoved(toPoint pos : CGPoint) {
         count = count + 1
-        print(count)
+        for point in posArr {
+            if(point.x == 0 || point.y == 0){}
+            else{
+                print(count.description+": "+point.x.description+" "+point.y.description)
+            }
+        }
         
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            posArr.append(pos)
             n.position = pos
             n.strokeColor = SKColor.white
             self.addChild(n)
@@ -34,37 +72,11 @@ class Scroller: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
-    override func sceneDidLoad() {
-        ball = childNode(withName: "ball") as! SKSpriteNode
-        
-       
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-            if let label = self.label {
-                label.alpha = 0.0
-                label.text = "Tap to Begin"
-                label.run(SKAction.fadeIn(withDuration: 2.0))
-            }
-   
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 0.75
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-
-    }
-    
+     
     override func didMove(to view: SKView) {
         
         physicsWorld.contactDelegate = self
@@ -91,7 +103,29 @@ class Scroller: SKScene, SKPhysicsContactDelegate {
         }
     }
 
-    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        //ball.position = posArr[posArr.count-1]
+        for p in posArr{
+            ball.position = p
+        }
+        // Initialize _lastUpdateTime if it has not already been
+        if self.lastUpdateTime == 0 {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+
     
     
 }
