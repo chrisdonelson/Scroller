86bcac6bb7fda0f95e6a70db84f19f02a2273e8f
Christopher Donelson <cmdonels@go.olemiss.edu>
2017-07-18 18:41:01 -0500

    added ability to draw line

--- Scroller/Scroller.swift
+++ Scroller/Scroller.swift
@@ -12,16 +12,14 @@ import SpriteKit
 class Scroller: SKScene, SKPhysicsContactDelegate {
     
     var ball: SKSpriteNode!
-    var firstPosition: CGPoint!
-    var lastPosition: CGPoint!
+    var line:SKShapeNode = SKShapeNode()
+    var path:CGMutablePath = CGMutablePath()
+    var initialPointSet:Bool = false
 
     override func sceneDidLoad() {
         ball = childNode(withName: "ball") as! SKSpriteNode
-
     }
-    
     override func didMove(to view: SKView) {
-        
         physicsWorld.contactDelegate = self
         ball.physicsBody?.affectedByGravity = true
         
@@ -29,26 +27,44 @@ class Scroller: SKScene, SKPhysicsContactDelegate {
         let borderBody = SKPhysicsBody(edgeLoopFrom: CGRect(x: -275.0, y: -374.0, width: 1334.0, height: 750.0))
         borderBody.friction = 0
         self.physicsBody = borderBody
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            firstPosition = touch.location(in: self)
+        eraseLine()
+        
+        // Starting position of line
+        for t in touches {
+            path.move(to: t.location(in: self))
         }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            lastPosition = touch.location(in: self)
+            drawLine(touch.location(in: self))
         }
     }
-
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        addLine()
+    }
     
+    func drawLine(_ point: CGPoint) {
+        path.addLine(to: point)
+        line.path = path
+    }
+    
+    func addLine() {
+        line.name = "line"
+        line.strokeColor = UIColor.white
+        line.lineWidth = 8
+        addChild(line)
+    }
+    
+    func eraseLine() {
+        self.childNode(withName: "line")?.removeFromParent()
+        initialPointSet = false
+        path = CGMutablePath()
+    }
     
 }
